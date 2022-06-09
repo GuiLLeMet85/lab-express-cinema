ca9fb950fa2ac1c4da00138a227446d9f838e3ba
Guillem B <guillembaracco@gmail.com>
2022-06-09 08:37:02 +0200

    iteration 2

--- seeds/movies.seed.js
+++ seeds/movies.seed.js
@@ -63,3 +63,18 @@ const movies = [{
         showtimes: ["13:00", "15:30", "18:00", "20:10", "22:40"]
     }
 ];
+
+const mongoose = require('mongoose')
+const movies = require('../models/Movie.model');
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/lab-express-cinema';
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
