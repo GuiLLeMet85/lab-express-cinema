2f354ce195787a63fbd787de51b5ad001fc0e9a2
Guillem B <guillembaracco@gmail.com>
2022-06-11 10:45:43 +0200

    lab done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/movies',async (req,res) => {
+    try {
+        const movies = await Movie.find();
+
+        res.render('movies', {movies});
+    }
+    catch (error) {
+        console.error(error);
+    }
+   
+});
+
+router.get('/movies/:movieId', (req, res, next) => {
+    const {movieId} = req.params;
+    Movie.findById(movieId)
+      .then( movie => {
+        res.render('./movie', {movie: movie}) 
+      })
+      .catch(err => {
+        console.log(`Error retrieving movie details --> ${err}`, )
+        next(err)
+      })
+  })
+
+
+
+
 module.exports = router;
+
